f613cffe67c9db328608c9a8dee50a1addd0c548
Rustam Iskenderov <Rustam Iskenderov>
2017-05-13 19:56:19 -0500

    better filtering

--- thermostat.ino
+++ thermostat.ino
@@ -141,113 +141,6 @@ const char* NAME_OFF_TIME = "wkdtimeoff";
 }
 }
 
-/*
-  class TimeNtp
-  {
-  public:
-
-    void setup()
-    {
-      udp.begin(localPort);
-      _timeZone = -6; // Central time (USA & Canada)
-    }
-
-    void loop()
-    {
-      //get a random server from the pool
-      WiFi.hostByName(ntpServerName, timeServerIP);
-
-      sendNTPpacket(timeServerIP); // send an NTP packet to a time server
-      // wait to see if a reply is available
-      delay(1000);
-
-      int cb = udp.parsePacket();
-      if (!cb) {
-        Serial.println("no packet yet");
-      }
-      else {
-        Serial.print("packet received, length=");
-        Serial.println(cb);
-        // We've received a packet, read the data from it
-        udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
-        //the timestamp starts at byte 40 of the received packet and is four bytes,
-        // or two words, long. First, esxtract the two words:
-        unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
-        unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
-        // combine the four bytes (two words) into a long integer
-        // this is NTP time (seconds since Jan 1 1900):
-        unsigned long secsSince1900 = highWord << 16 | lowWord;
-        Serial.print("Seconds since Jan 1 1900 = " );
-        Serial.println(secsSince1900);
-
-        // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
-        const unsigned long seventyYears = 2208988800UL;
-        // subtract seventy years:
-        unsigned long epoch = secsSince1900 - seventyYears;
-
-        // print the hour, minute and second:
-        Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
-        unsigned long hours = (epoch  % 86400L) / 3600; // print the hour (86400 equals secs per day)
-        unsigned long minutes = (epoch  % 3600) / 60; // print the minute (3600 equals secs per minute)
-        unsigned long seconds = (epoch % 60); // print the second
-
-        int hours_local = hours + _timeZone;
-        Serial.print(hours_local);
-        Serial.print(":");
-        Serial.println(minutes);
-      }
-      // wait ten seconds before asking for the time again
-      delay(10000);
-    }
-
-    int _timeZone;
-
-  private:
-    // send an NTP request to the time server at the given address
-    unsigned long sendNTPpacket(IPAddress& address)
-    {
-      Serial.println("sending NTP packet...");
-      // set all bytes in the buffer to 0
-      memset(packetBuffer, 0, NTP_PACKET_SIZE);
-      // Initialize values needed to form NTP request
-      // (see URL above for details on the packets)
-      packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-      packetBuffer[1] = 0;     // Stratum, or type of clock
-      packetBuffer[2] = 6;     // Polling Interval
-      packetBuffer[3] = 0xEC;  // Peer Clock Precision
-      // 8 bytes of zero for Root Delay & Root Dispersion
-      packetBuffer[12]  = 49;
-      packetBuffer[13]  = 0x4E;
-      packetBuffer[14]  = 49;
-      packetBuffer[15]  = 52;
-
-      // all NTP fields have been given values, now
-      // you can send a packet requesting a timestamp:
-      udp.beginPacket(address, 123); //NTP requests are to port 123
-      udp.write(packetBuffer, NTP_PACKET_SIZE);
-      udp.endPacket();
-    }
-
-    const unsigned int localPort = 2390;      // local port to listen for UDP packets
-
-    // Don't hardwire the IP address or we won't get the benefits of the pool.
-    //    Lookup the IP address for the host name instead
-    //IPAddress timeServer(129, 6, 15, 28); // time.nist.gov NTP server
-    IPAddress timeServerIP; // time.nist.gov NTP server address
-    const char* ntpServerName = "time.nist.gov";
-
-    static const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
-
-    byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
-
-    // A UDP instance to let us send and receive packets over UDP
-    WiFiUDP udp;
-
-
-  };
-*/
-
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Enter your WiFi network SSID
@@ -262,11 +155,10 @@ const char* password = "";
 const int PORT_NETWORK = 8150;
 
 WiFiServer server(PORT_NETWORK);
-
+WiFiUDP ntpUDP;
 
 Thermostat thermostat;
-//TimeNtp timeNtp;
-WiFiUDP ntpUDP;
+
 NTPClient timeClient(ntpUDP);
 
 void setup() {
@@ -293,8 +185,7 @@ void setup() {
   server.begin();
 
   thermostat.setup();
-  //timeNtp.setup();
-  //timeNtp.loop();
+
   timeClient.begin();
   timeClient.setTimeOffset(-6 * 60 * 60);
   timeClient.setUpdateInterval(120000);
@@ -337,35 +228,40 @@ bool getValue(int &value, const char* name, const String &request)
   return true;
 }
 
-const uint8_t SAMPLES_COUNT = 20;
-ArduinoMedianFilter<uint16_t, uint8_t, SAMPLES_COUNT> temperatureFilter;
+const uint16_t SAMPLES_COUNT = 300;
+ArduinoMedianFilter<uint16_t, SAMPLES_COUNT> temperatureFilter;
+
+
+double celciusToFahrenheit(double celsius)
+{
+  return celsius * 9.0 / 5.0 + 32;
+}
 
 double getTemperature()
 {
-  temperatureFilter.clear();
+  uint16_t ntcRef; // digital value on NTC thermistor
+
+  for (int i = 0; i < SAMPLES_COUNT; i++)
+  {
+    temperatureFilter.set(i, analogRead(A0));
+    //delay(1);
+  }
+
+  temperatureFilter.getAverage(30, ntcRef);
 
   int gndRef = 5; // value on A0 pin when it's connected to GND
   int vccRef = 985; // value on A0 pin when it's connected to 3.3V
   double Vcc = 3.277; // real voltage between 3.3 and GND pins, use multimeter to measure.
 
-  uint16_t ntcRef; // digital value on NTC thermistor
-
-  for (int i = 0; i < SAMPLES_COUNT; i++) {
-    temperatureFilter.add(analogRead(A0));
-    delay(1);
-  }
-
-  temperatureFilter.getMedian(ntcRef);
-
   ntcRef = constrain(ntcRef, gndRef, vccRef);
 
-  // converting to real voltage
-  double Vntc = Vcc * (ntcRef - gndRef) / (vccRef - gndRef);
+  // Real voltage on analogue pin
+  double Vout = Vcc * (ntcRef - gndRef) / (vccRef - gndRef);
 
   double R0 = 20210.0; // resistor in voltage divider, use multimeter to measure
 
   // calculating current resistance of thermistor
-  double Rntc = (R0 * Vntc) / (Vcc - Vntc);
+  double Rntc = (R0 * Vout) / (Vcc - Vout);
 
   Rntc = log(Rntc);
   double temperature = 1.0 / (0.001129148 + (0.000234125 + (0.0000000876741 * Rntc * Rntc )) * Rntc );
@@ -398,7 +294,7 @@ void loop() {
   // Check if a client has connected
   WiFiClient client = server.available();
   if (!client) {
-    gTemperature = getTemperature();
+    gTemperature = celciusToFahrenheit(getTemperature());
     Serial.println(gTemperature);
     timeClient.update();
     return;
@@ -484,7 +380,7 @@ char strElement[90];
 sprintf(strElement, "<p>Time: %s</p>", timeClient.getFormattedTime().c_str());
 client.print(strElement);
 
-sprintf(strElement, "<p>Temperature: %dC</p><br>", (int)gTemperature);
+sprintf(strElement, "<p>Temperature: %dF</p><br>", (int)gTemperature);
 client.println(strElement);
 
 sprintf(strElement, "<input name=state type=radio value=cool %s />Cool<br><br>", (thermostat._state == Thermostat::State::Cool) ? strChecked : "");
